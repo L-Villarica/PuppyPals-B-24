f6126208a9c8c21d687a50760ff2bc7e4bca2641
Lance Villarica <lanceoliver_villarica@yahoo.com>
2023-06-15 17:28:36 -0700

    Added array

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-
-
+import {puppyList} from './data.js'
+import { useState } from 'react'
 function App() {
-
-
+  const [puppies, setPuppies] = useState(puppyList)
+  console.log(puppies)
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+{ 
+   puppies.map((puppy) => {
+     return <p key={puppy.id}>{puppy.name}</p>
+   })
+}
       </div>
     </>
   )
